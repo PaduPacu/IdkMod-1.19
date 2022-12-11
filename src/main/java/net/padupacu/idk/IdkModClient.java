f0d1ea5576decd3346167fe61de69b833d9cb591
PaduPacu <padugames@gmail.com>
2022-12-11 11:19:13 -0300

    Initial commit

---
+++ src/main/java/net/padupacu/idk/IdkModClient.java
@@ -0,0 +1,10 @@
+package net.padupacu.idk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IdkModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/padupacu/idk/idk.java
@@ -0,0 +1,15 @@
+package net.padupacu.idk;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class idk implements ModInitializer {
+	public static final String MOD_ID = "idk";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
